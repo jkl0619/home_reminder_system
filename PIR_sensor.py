fd1052a56c0db7b9cdf044f46794794d1c3b19af
millerc3 <millerc3@vt.edu>
2018-05-01 02:33:13 +0000

    Update PIR_sensor.py

--- PIR_sensor.py
+++ PIR_sensor.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 import paho.mqtt.client as mqtt
+import paho.mqtt.publish as publish
 import pin_setup
 import time
 import database
@@ -9,23 +10,19 @@ info = database.system_information
 
 #GPIO setup
 pins = pin_setup.pins
-GPIO.setmode(pins['pinmode'])
+GPIO.setmode(GPIO.BOARD)
 GPIO.setup(pins['data_in'], GPIO.IN)
+GPIO.setup(pins['led_pin'], GPIO.OUT)
 
 #MQTT setup
-client_name = 'motion_sensor_0'
-topic = "ROOM_STATUS"
-Client(client_id="", clean_session=True, userdata=None, protocol=MQTTv311, transport="tcp")
-client =mqtt.Client(client_name)
-client.connect(host, port=1883, keepalive=60, bind_address="")
-subscribe(topic, qos=0)
-
+mqttc = mqtt.Client()
+mqttc.connect("m2m.eclipse.org", 1883)
 
 #calibration
-print("calibrating sensor\n";
-calibrationTime = 30;    		#data sheet asks for a 10s to 60s calibration time
+print("calibrating sensor\n");
+calibrationTime = 3;    		#data sheet asks for a 10s to 60s calibration time
 for x in range (0, calibrationTime):
-	print(".\n");
+	print(str(x) + "\n");
 	time.sleep(1)
 print("done\n");
 time.sleep(1)
@@ -35,29 +32,30 @@ print("SENSOR ACTIVE\n");
 #motion detection
 inRoom = False
 sent = False
+lights = False
 while 1:			#constantly check if the PIR sensor has seen motion
-	if GPIO.input(pins['data_in']) == GPIO.HIGH):			#sensor will display a high value in the event that there is any motion detected
-		time.sleep(.5)										#slight delay since the sensor will have a high value for about .3s
-		if inRoom == False									#if they ARE NOT currently in the room, it means they are passing the sensor to ENTER the room
+	if GPIO.input(pins['data_in']) == GPIO.HIGH:			#sensor will display a high value in the event that there is any motion detected
+		time.sleep(7.5)										#slight delay since the sensor will have a high value for about 5s
+		if inRoom == False:									#if they ARE NOT currently in the room, it means they are passing the sensor to ENTER the room
 			sent = False
 			inRoom = True	
-			print('%s has entered the room, turning on the lights', info['name'])
-		if inRoom:											#if they ARE currently in the room, it means they are passing the sensor again to LEAVE the room 		
+			print(info['name'] + ' has entered the room, turning on the lights')
+		elif inRoom:											#if they ARE currently in the room, it means they are passing the sensor again to LEAVE the room 		
 			Sent = False
 			inRoom = False	
-			print('%s has left the room, turning off the lights', info['name'])
+			print(info['name'] + ' has left the room, turning off the lights')
 	
 		
 		
 	if inRoom and not sent:						#while the user is in the room, the lights should be on
+		print('sending roomstatus to server: room_0 entered')
 		sent = True
-		client.publish(topic, "Room Status: inside room 0")					#send a message that the user is now in room 0
-		#####
-		# use lights.py or whatever to turn on the lights in this room
-		#####
+		decoded_message = 'User has entered room 0'
+		publish.single("netapphome/roomstatus/0", decoded_message, hostname="m2m.eclipse.org")					#send a message that the user is now in room 0
+		GPIO.output(pins['led_pin'], GPIO.HIGH)
+		lights = True
 		
-	if not inRoom:
-		#####
-		# use lights.py or whatever to turn off the lights in this room
-		#####
-		
+	elif not inRoom and lights:
+                lights = False
+                GPIO.output(pins['led_pin'], GPIO.LOW)
+
