928d5c33d7c1dc6b95400900a63bbc07dbc5ffc7
millerc3 <millerc3@vt.edu>
2018-05-07 19:54:48 +0000

    Update speaker.py

--- speaker.py
+++ speaker.py
@@ -2,7 +2,11 @@ from gtts import gTTS
 import paho.mqtt.client as mqtt
 import os
 
-topic = 'netapphome/alert/0'
+topic = 'netapphome/alert/0'        #each pi will have a different number at the 
+				    #end of each topic, it will be hard coded
+				    #and strictly for that pi.  PIR_sensor.py
+				    #will also need to change the topic it is
+				    #using with the same idea
 
 #message callback function
 def on_message(client, userdata, message):
@@ -10,6 +14,7 @@ def on_message(client, userdata, message):
 	tts = gTTS(text=serverMessage, lang='en', slow=False)		#serverMessage into google's text to speech data type
 	tts.save("alert.mp3")										#save the tts file 
 	os.system("omxplayer alert.mp3")							#play the mp3 file
+	print(serverMessage + '\n')
 	
 def on_connect(client, userdata, flags, rc):
 	client.subscribe(topic)
